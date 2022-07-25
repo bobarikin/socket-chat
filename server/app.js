28436d618407149711aaf5093c0a4797b4e3e22f
Artyom <a.s.bobarikin@gmail.com>
2022-07-25 14:32:37 +0300

    add mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import { createServer } from 'http'
+import { MONGODB_URI } from './config.js'
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
 const server = createServer(app)
+
+try {
+    mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    console.log('🚀 Connected')
+} catch (e) {
+    console.log('DB connected error')
+    process.exit(1)
+}
 
 server.listen(PORT, () => 
     console.log(`🚀 Server has been started on port ${PORT}...`)
---
+++ server/config.js
@@ -0,0 +1,1 @@
+export const MONGODB_URI = "mongodb+srv://Artyom:12345@cluster0.uam80bl.mongodb.net/app?retryWrites=true&w=majority"
